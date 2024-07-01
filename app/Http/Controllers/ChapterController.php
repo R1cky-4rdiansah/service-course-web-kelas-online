6bac963295376a7cf98b85b16f78367736615b18
R1cky <cuah2ncok222@gmail.com>
2024-07-01 10:48:28 +0700

    crud chapter

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -9,6 +9,20 @@ use Illuminate\Support\Facades\Validator;
 
 class ChapterController extends Controller
 {
+    public function index(Request $request)
+    {
+        $courseId = $request->query("course_id");
+        $chapters = Chapter::query();
+
+        $chapters->when($courseId, function ($query) use ($courseId) {
+            $query->where("course_id", $courseId);
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $chapters->get()
+        ], 200);
+    }
     public function create(Request $request)
     {
         $role = [
@@ -41,6 +55,89 @@ class ChapterController extends Controller
         return response()->json([
             'status' => 'success',
             'data' => $chapter
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $role = [
+            'name' => 'required|string',
+            'course_id' => 'required|integer'
+        ];
+
+        $data = $request->all();
+
+        $valid = Validator::make($data, $role);
+
+        if ($valid->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $valid->error()
+            ], 400);
+        }
+
+        $chapter = Chapter::find($id);
+
+        if (!$chapter) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Data chpater tidak ditemukan'
+            ], 404);
+        }
+
+        $courseId = $request->input('course_id');
+
+        if ($courseId) {
+            $course = Course::find($courseId);
+            if (!$course) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Data kursus tidak ditemukan'
+                ], 404);
+            }
+        }
+
+        $chapter->fill($data)->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $chapter,
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $chapter = Chapter::find($id);
+
+        if (!$chapter) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Data chapter '
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $chapter
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $chapter = Chapter::find($id);
+
+        if (!$chapter) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Data chapter tidak ditemukan'
+            ], 404);
+        }
+
+        $chapter->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data chapter telah dihapus'
         ], 200);
     }
 }
